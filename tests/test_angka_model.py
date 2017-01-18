a597b734766da8076a051405b0c30608176025d2
ihfazhillah <mihfazhillah@gmail.com>
2017-01-18 17:36:40 +0700

    data adalah constans harus uppercase

--- tests/test_angka_model.py
+++ tests/test_angka_model.py
@@ -1,11 +1,11 @@
 from qaamus2.models.angka import AngkaModel
 from nose import tools
 
-data = {'indo': 12,
+DATA = {'indo': 12,
         'arab': 'اثنا عشر',
         'url': 'http://coba.saja/angka/12'}
 
 def test_angka_model_representation():
-    angka = AngkaModel(**data)
+    angka = AngkaModel(**DATA)
     expected = '<AngkaModel -12->'
     tools.eq_(expected, angka.__repr__())
