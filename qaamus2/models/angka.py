1155094a86e4734d9bc50951d6099e8bd2d327d4
ihfazhillah <mihfazhillah@gmail.com>
2017-01-18 17:49:08 +0700

    raise valueerror kalau indo adalah string

--- qaamus2/models/angka.py
+++ qaamus2/models/angka.py
@@ -12,7 +12,15 @@ class AngkaModel(QaamusBaseModel):
     """
 
     def __init__(self, indo, arab, url):
-        self.indo = indo
+        self.indo = self._check_angka(indo)
         self.arab = arab
         self.url = url
         super(AngkaModel, self).__init__(self.indo)
+
+    @classmethod
+    def _check_angka(cls, indo):
+        """Angka harus berupa int, atau string angka"""
+        if not isinstance(indo, int):
+            raise ValueError(("Angka harus berupa int, "
+                              "atau string berisi digit"))
+        return indo
