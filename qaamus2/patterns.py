f31efae91b21308f29fc7aba238881606ae0b7a9
ihfazhillah <mihfazhillah@gmail.com>
2017-01-18 15:32:17 +0700

    menambah pattern lists

---
+++ qaamus2/patterns.py
@@ -0,0 +1,12 @@
+"""Pattern lists"""
+import re
+
+
+IDAR = re.compile("""<div class=lateef2>(.+?)</div>.+<em.+?>(.+?)</em>.+?<div class="panel-footer">(.+?)</div>""")
+ANGKA_PEGON = re.compile("""<div class=lateef2>(.+?)</div>""")
+BERHUBUNGAN = re.compile("""<td.+?><a href="(.+?)">(.+?)</a></td><td class="lateef".+?>(.+?</div>)</td>""")
+TAGS = re.compile(r"<.+?>")
+PAGINATION = re.compile(r"""http://qaamus\.com/indonesia-arab[\w\/]+?""")
+PAGINATION_URL = re.compile(r"""<li> ?<a href='(http://qaamus\.com/indonesia-arab[\w+\/]+?) '>(?!Next)""")
+PAGE_IN_URL = re.compile(r'\d+')
+CURRENT_PAGE = re.compile(r'<a href=#>(\d+)</a>')
