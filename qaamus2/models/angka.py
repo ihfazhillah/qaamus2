4591e68c815cb6afd2b6b221292c19bf624aab7d
ihfazhillah <mihfazhillah@gmail.com>
2017-01-18 17:39:23 +0700

    menambah dokumentasi dan memanggil base class dengan super

--- qaamus2/models/angka.py
+++ qaamus2/models/angka.py
@@ -1,9 +1,18 @@
+"""Angka model modul"""
 from qaamus2.models.basemodel import QaamusBaseModel
 
 
 class AngkaModel(QaamusBaseModel):
+    """Merepresentasikan angka model,
+
+    params:
+        - indo : indonesia
+        - arab : arab
+        - url : url pencarian
+    """
 
     def __init__(self, indo, arab, url):
         self.indo = indo
         self.arab = arab
         self.url = url
+        super(AngkaModel, self).__init__(self.indo)
