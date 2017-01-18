00988ae15f25a252c8c4003e28a112e16b4ee3c0
ihfazhillah <mihfazhillah@gmail.com>
2017-01-18 18:56:35 +0700

    indo tidak boleh integer (fix)

--- qaamus2/models/munawwir_berhub.py
+++ qaamus2/models/munawwir_berhub.py
@@ -6,6 +6,13 @@ class MunawwirBerhubModel(QaamusBaseModel):
     def __init__(self, indo, arab, url):
         super(MunawwirBerhubModel, self).__init__(indo)
 
-        self.indo = indo
+        self.indo = self._validate_indo(indo)
         self.arab = arab
         self.url = url
+
+    @classmethod
+    def _validate_indo(cls, indo):
+        if isinstance(indo, int):
+            raise ValueError("indo tidak boleh integer")
+
+        return indo
