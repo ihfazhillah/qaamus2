3ddbd2b2c6e9984fd1c79df251c1d7935b6406d8
ihfazhillah <mihfazhillah@gmail.com>
2017-01-18 18:49:46 +0700

    menambah test untuk munawwir berhub model

---
+++ tests/test_munawwir_berhub.py
@@ -0,0 +1,11 @@
+from qaamus2.models import MunawwirBerhubModel
+from nose import tools
+
+DATA = {'indo': 'mengambil tongkat',
+        'arab': 'عَصِيَ',
+        'url': 'http://qaamus.com/indonesia-arab/mengambil+tongkat/1'}
+
+def test_representation():
+    munawwir_berhub = MunawwirBerhubModel(**DATA)
+    expected = '<MunawwirBerhubModel -mengambil tongkat->'
+    tools.eq_(expected, munawwir_berhub.__repr__())
