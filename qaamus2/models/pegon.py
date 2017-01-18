39c0377168cc9560e3bbc0576042f3f088b308fc
ihfazhillah <mihfazhillah@gmail.com>
2017-01-18 18:40:01 +0700

    indo tidak boleh int

--- qaamus2/models/pegon.py
+++ qaamus2/models/pegon.py
@@ -7,6 +7,16 @@ class PegonModel(QaamusBaseModel):
     def __init__(self, indo, arab, url):
         super(PegonModel, self).__init__(indo)
 
-        self.indo = indo
+        self.indo = self._validate_indo(indo)
         self.arab = arab
         self.url = url
+
+    @classmethod
+    def _validate_indo(cls, indo):
+        """Validasi indo, tidak bole berupa int atau
+        string berisi digit"""
+
+        if isinstance(indo, int):
+            raise ValueError("""Value param indo tidak bole berisi int,
+                                atau string berisi digit""")
+        return indo
--- tests/test_pegon_model.py
+++ tests/test_pegon_model.py
@@ -10,3 +10,7 @@ def test_representation():
     pegon = PegonModel(**DATA)
     expected = '<PegonModel -tongkat->'
     tools.eq_(expected, pegon.__repr__())
+
+@tools.raises(ValueError)
+def test_raise_value_error_ketika_dimasukkan_integer():
+    pegon = PegonModel(indo=12, arab='عربي', url='http://qaamus.com')
