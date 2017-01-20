520b7776a232581f7be4ea62e280135ac3226242
ihfazhillah <mihfazhillah@gmail.com>
2017-01-20 20:52:17 +0700

    Munawwir object bisa memberikan hasil

--- qaamus2/scraper.py
+++ qaamus2/scraper.py
@@ -4,6 +4,7 @@ from qaamus2.models.pegon import PegonModel
 from qaamus2.parsers import Parser
 from qaamus2.models.munawwir_berhub_collections import MunawwirBerhubModelCollections
 from qaamus2.models.munawwir_berhub import MunawwirBerhubModel
+from qaamus2.models.munawwir import MunawwirModel
 
 class AngkaScraper(object):
     
@@ -71,3 +72,14 @@ class MunawwirScraper(object):
         berhubungan_iter = (MunawwirBerhubModel(x[1], x[2], x[0]) for x in berhubungan)
 
         return MunawwirBerhubModelCollections(berhubungan_iter)
+
+    def hasil(self):
+        parser = Parser(self.response)
+        arab, baca, source = parser.utama()
+
+        return MunawwirModel(indo=self.indo,
+                             arab=arab,
+                             baca=baca,
+                             sumber=source,
+                             url=self.url,
+                             berhubungan=self.berhubungan)
--- tests/test_scraper_munawwir.py
+++ tests/test_scraper_munawwir.py
@@ -38,3 +38,21 @@ def test_berhubungan(obj):
 
     tools.eq_(berhubungan.__dict__, expected)
     tools.ok_(isinstance(munawwir.berhubungan, MunawwirBerhubModelCollections))
+
+@patch('qaamus2.scraper.requests.get')
+@patch.object(MunawwirScraper, 'berhubungan', spec=MunawwirBerhubModelCollections)
+def test_hasil(munawwir_mock,obj):
+    obj.return_value.text = RESPONSE
+
+    berhubungan = MunawwirBerhubModelCollections((x for x in range(3)))
+    munawwir_mock.return_value = berhubungan
+
+    munawwir = MunawwirScraper('lari')
+
+    expected = MunawwirModel(indo='lari', arab='عَدْوٌ ، جَرْيٌ ، عَدَا - يَعْدُوْ ، جَرَى - يَجْرِي',
+                             baca="adwun, jaryun, 'adaa - ya'duu, jaroo - yajrii",
+                             sumber='Data diambil dari Kamus Al-Munawwir Edisi Indonesia Arab pada halaman 499',
+                             url='http://qaamus.com/indonesia-arab/lari/1',
+                             berhubungan=munawwir_mock)
+
+    tools.eq_(munawwir.hasil().__dict__, expected.__dict__)
