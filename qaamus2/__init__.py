0e3892931cd72a819034b9f79acab154f32664a1
ihfazhillah <mihfazhillah@gmail.com>
2017-02-07 12:02:23 +0700

    raise typeerror ketika bukan subclass dari BaseScraper

--- qaamus2/__init__.py
+++ qaamus2/__init__.py
@@ -1,3 +1,6 @@
 
 class Qaamus(object):
     scrapers = []
+
+    def register_scraper(self, scraper):
+        raise TypeError("%s bukan subclass dari BaseScraper" % scraper.__name__)
--- tests/test_registering_scraper.py
+++ tests/test_registering_scraper.py
@@ -5,3 +5,8 @@ from qaamus2 import Qaamus
 
 def test_qaamus_empty_scraper():
     tools.eq_(Qaamus.scrapers, [])
+
+@tools.raises(TypeError)
+def test_qaamus_register_scraper_failed():
+    class BukanScraper: pass
+    Qaamus.register_scraper(BukanScraper)
