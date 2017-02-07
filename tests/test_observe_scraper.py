dc6f890c33a52e7199e30bb1fc4121306a396088
ihfazhillah <mihfazhillah@gmail.com>
2017-02-07 20:30:30 +0700

    menghapus setup_module dan teardown_module, ternyata di semua test jadinya

--- tests/test_observe_scraper.py
+++ tests/test_observe_scraper.py
@@ -1,13 +1,6 @@
 from qaamus2 import Qaamus
-from nose import tools
+from nose import tools, with_setup
 from qaamus2.scraper import AngkaScraper, PegonScraper
-
-def setup_module():
-    Qaamus.register_scraper(AngkaScraper)
-    Qaamus.register_scraper(PegonScraper)
-
-def teardown_module():
-    Qaamus.scrapers = []
 
 def test_get_angka_scraper():
     angka = Qaamus('angka', 1234)
