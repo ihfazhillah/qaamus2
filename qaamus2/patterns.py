72ed29587595b427f2da88dc1c94653be8b36bf7
ihfazhillah <mihfazhillah@gmail.com>
2017-02-19 12:05:28 +0700

    tambahkan pattern untuk bila hasil didapatkan dari bing trans

--- qaamus2/patterns.py
+++ qaamus2/patterns.py
@@ -3,6 +3,7 @@ import re
 
 
 IDAR = re.compile("""<div class=lateef2>(.+?)</div>.+<em.+?>(.+?)</em>.+?<div class="panel-footer">(.+?)</div>""")
+IDAR_BING = re.compile("""<div class=lateef2>(.+?)</div>.+?<div class="panel-footer">(.+?)</div>""")
 ANGKA_PEGON = re.compile("""<div class=lateef2>(.+?)</div>""")
 BERHUBUNGAN = re.compile("""<td.+?><a href="(.+?)">(.+?)</a></td><td class="lateef".+?>(.+?</div>)</td>""")
 TAGS = re.compile(r"<.+?>")
